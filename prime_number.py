f9e99ffe91eebd62ac503d4c0797004145600768
Tomohiro-Morishita <t.morishita.24.10.1589@4nonome.com>
2024-10-10 21:11:58 +0900

    制御構文課題１

--- prime_number.py
+++ prime_number.py
@@ -2,3 +2,13 @@ a = input("aの値を入力: ")
 b = input("bの値を入力: ")
 
 # TODO
+count = 2
+numbers = [int(a),int(b)]
+for number in numbers:
+    while count < number:
+        if number % count == 0:
+            print(f"{number}は素数ではありません")
+            break
+        count += 1
+    if count == number:
+        print(f"{number}は素数です")
