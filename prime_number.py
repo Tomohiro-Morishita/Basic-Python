e04b9b22cd6a675989dc4b0247ae855204c9c14c
Tomohiro-Morishita <t.morishita.24.10.1589@4nonome.com>
2024-10-12 11:33:12 +0900

    訂正した素数判定プログラム

--- prime_number.py
+++ prime_number.py
@@ -2,13 +2,13 @@ a = input("aの値を入力: ")
 b = input("bの値を入力: ")
 
 # TODO
-count = 2
 numbers = [int(a),int(b)]
 for number in numbers:
-    while count < number:
+    count = 2
+    while count <= number ** (1 / 2):
         if number % count == 0:
             print(f"{number}は素数ではありません")
             break
         count += 1
-    if count == number:
+    if count > number ** (1 / 2):
         print(f"{number}は素数です")
