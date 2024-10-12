683183736c2a22a7ebe2429b20d297603d79ca7b
Tomohiro-Morishita <t.morishita.24.10.1589@4nonome.com>
2024-10-12 17:06:01 +0900

    積分問題の解答

--- trapezoidal_integral.py
+++ trapezoidal_integral.py
@@ -1,5 +1,15 @@
 from math import sin
+from math import pi
 # --example--
 # print(sin(0))
 # >>> 0
-# -----------
+# ----------
+a = 0
+b = (1 / 2) * pi
+N = 100
+h = ( b - a) / N
+sum = 0
+for k in range( 1, N):
+    sum = sum + sin(a + (k - 1) * h) + sin(a + k * h)
+answer = (h / 2) * sum
+print(answer)
