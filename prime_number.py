d851a4120de5231cf5a9c9fbd5e4b97287fedffd
Tomohiro-Morishita <t.morishita.24.10.1589@4nonome.com>
2024-10-14 17:24:53 +0900

    素数判定関数エクストラ問題

--- prime_number.py
+++ prime_number.py
@@ -2,7 +2,12 @@ a = input("aの値を入力: ")
 b = input("bの値を入力: ")
 
 # TODO
+class NumberError(Exception):
+    pass
+
 def prime_number(n):
+    if n <= 1:
+        raise NumberError(f"{n}は適切な入力ではありません。")
     count = 2
     while count <= n ** (1 / 2):
         if n % count == 0:
