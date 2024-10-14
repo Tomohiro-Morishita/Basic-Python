5fa42587ace3f7520aa994a42a226e185240dff0
Tomohiro-Morishita <t.morishita.24.10.1589@4nonome.com>
2024-10-14 11:19:44 +0900

    誤った課題の削除

--- euclid.py
+++ euclid.py
@@ -2,18 +2,3 @@ a = input("a の値を入力: ")
 b = input("b の値を入力: ")
 
 # TODO
-a = int(a)
-b = int(b)
-if a >= b:
-    high = a
-    low = b
-else :
-    high = b
-    low = a
-
-while low != 0:
-    tmp= low
-    low = high % low
-    high = tmp
-print("%dと%dの最大公約数は%d",a, b, high)
-
