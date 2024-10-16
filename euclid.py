866b2cd930a4675a92cb2a68a615d74e5813ada9
Tomohiro-Morishita <t.morishita.24.10.1589@4nonome.com>
2024-10-16 11:27:55 +0900

    問３，４

--- euclid.py
+++ euclid.py
@@ -2,17 +2,23 @@ a = input("a の値を入力: ")
 b = input("b の値を入力: ")
 
 # TODO
-a = int(a)
-b = int(b)
-if a >= b:
-    high = a
-    low = b
-else :
-    high = b
-    low = a
 
-while low != 0:
-    tmp= low
-    low = high % low
-    high = tmp
-print("%dと%dの最大公約数は%d"%(a, b, high))
+def euclid(a, b):
+    a = int(a)
+    b = int(b)
+    if a >= b:
+        high = a
+        low = b
+    else:
+        high = b
+        low = a
+    while low != 0:
+        low, high = high % low, low
+    return high
+
+def mutually_prime(a, b):
+    return euclid(a, b) == 1
+
+print(euclid(a, b))
+print(mutually_prime(a, b))
+
